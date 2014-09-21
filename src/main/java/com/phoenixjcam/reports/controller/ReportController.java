cb7a8a2de5e0d0b5b51b3660544ff75a223f45ad
BartBien <phoenixjcam@gmail.com>
2014-09-21 11:34:22 +0200

    test commit 2

--- src/main/java/com/phoenixjcam/reports/controller/ReportController.java
+++ src/main/java/com/phoenixjcam/reports/controller/ReportController.java
@@ -9,6 +9,7 @@ import org.springframework.web.servlet.ModelAndView;
 public class ReportController
 {
 	// 
+	// test commit 2 
 	@RequestMapping(value = { "/", "/home" }, method = RequestMethod.GET)
 	public ModelAndView home()
 	{
